14a71b8db21ed3da44883e7ed14fdae528ba1258
Shahrukh Ahmed <shahrukh.ahmed@nagarro.com>
2020-11-04 18:27:30 +0530

    kv test

--- kv-demo/kv-demo/Program.cs
+++ kv-demo/kv-demo/Program.cs
@@ -12,7 +12,7 @@ namespace kv_demo
             //string keyVaultName = Environment.GetEnvironmentVariable("ad-test-keyvault");
             /////https://ad-test-keyvault.vault.azure.net/secrets/Hello/274562837c414d4d8d8d5ef542f67d5c
             //var kvUri = $"https://{keyVaultName}.vault.azure.net";
-            var kvUri = "https://kv-az-demo-web.vault.azure.net/";
+            var kvUri = "https://ad-test-keyvault.vault.azure.net/";
             var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
             var secret_Value = await client.GetSecretAsync("Hello");
             Console.WriteLine($"Secret text - {secret_Value.Value.Value}!");
